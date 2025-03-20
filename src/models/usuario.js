aac54a47e3209f8e3c78c45ec26ee720a179f103
Alishon <alison.martinez@soy.sena.edu.co>
2025-03-20 18:29:01 -0500

    Mensaje del commit

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -7,12 +7,24 @@ const Usuario = sequelize.define('Usuario', {
         autoIncrement: true, 
         primaryKey: true,
         field: 'IDUsuario' 
-    },
-    Nombre: { type: DataTypes.STRING, allowNull: false },
-    Identificacion: { type: DataTypes.STRING, allowNull: false, unique: true },
-    CorreoElectronico: { type: DataTypes.STRING, allowNull: false, unique: true },
-    Contrasena: { type: DataTypes.STRING, allowNull: false },
-    RolID: { type: DataTypes.INTEGER, allowNull: false }
+    }, Nombre: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    }, Identificacion: { 
+        type: DataTypes.STRING, 
+        allowNull: false, 
+        unique: true 
+    }, CorreoElectronico: { 
+        type: DataTypes.STRING, 
+        allowNull: false, 
+        unique: true 
+    }, Contrasena: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    },RolID: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false 
+    }
 }, { 
     timestamps: false,  
     tableName: 'Usuario', 
